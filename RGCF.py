191faa4308bd8ea8483dea38fd8e3957a223071f
hfutmars <59786976+hfutmars@users.noreply.github.com>
2020-01-25 10:41:33 +0800

    Add files via upload

---
+++ RGCF.py
@@ -0,0 +1,278 @@
+import tensorflow as tf
+import os
+import sys
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+from utility.helper import *
+from utility.batch_test import *
+
+class RGCF(object):
+    def __init__(self, data_config, pretrain_data):
+        # argument settings
+        self.adj_type = args.adj_type
+        self.alg_type = args.alg_type
+
+        self.pretrain_data = pretrain_data
+
+        self.n_users = data_config['n_users']
+        self.n_items = data_config['n_items']
+
+        self.n_fold = 100
+
+        self.norm_adj = data_config['norm_adj']
+        self.n_nonzero_elems = self.norm_adj.count_nonzero()
+
+        self.lr = args.lr
+
+        self.emb_dim = args.embed_size
+        self.batch_size = args.batch_size
+
+        self.weight_size = eval(args.layer_size)
+        self.n_layers = len(self.weight_size)
+
+        self.regs = eval(args.regs)
+        self.decay = self.regs[0]
+
+        self.verbose = args.verbose
+
+        # placeholder definition
+        self.users = tf.placeholder(tf.int32, shape=(None,))
+        self.pos_items = tf.placeholder(tf.int32, shape=(None,))
+        self.neg_items = tf.placeholder(tf.int32, shape=(None,))
+
+        self.node_dropout_flag = args.node_dropout_flag
+        self.node_dropout = tf.placeholder(tf.float32, shape=[None])
+        self.mess_dropout = tf.placeholder(tf.float32, shape=[None])
+
+        self.weights = self._init_weights()
+
+        self.ua_embeddings, self.ia_embeddings = self._create_ngcf_embed()
+
+        self.u_g_embeddings = tf.nn.embedding_lookup(self.ua_embeddings, self.users)
+        self.pos_i_g_embeddings = tf.nn.embedding_lookup(self.ia_embeddings, self.pos_items)
+        self.neg_i_g_embeddings = tf.nn.embedding_lookup(self.ia_embeddings, self.neg_items)
+
+        self.batch_ratings = tf.matmul(self.u_g_embeddings, self.pos_i_g_embeddings, transpose_a=False, transpose_b=True)
+
+        self.mf_loss, self.emb_loss, self.reg_loss = self.create_bpr_loss(self.u_g_embeddings,
+                                                                          self.pos_i_g_embeddings,
+                                                                          self.neg_i_g_embeddings)
+        self.loss = self.mf_loss + self.emb_loss + self.reg_loss
+
+        self.opt = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss)
+
+    def _init_weights(self):
+        all_weights = dict()
+
+        initializer = tf.contrib.layers.xavier_initializer()
+
+        all_weights['user_embedding'] = tf.Variable(initializer([self.n_users, self.emb_dim]), name='user_embedding')
+        all_weights['item_embedding'] = tf.Variable(initializer([self.n_items, self.emb_dim]), name='item_embedding')
+
+        return all_weights
+
+    def _split_A_hat(self, X):
+        A_fold_hat = []
+
+        fold_len = (self.n_users + self.n_items) // self.n_fold
+        for i_fold in range(self.n_fold):
+            start = i_fold * fold_len
+            if i_fold == self.n_fold -1:
+                end = self.n_users + self.n_items
+            else:
+                end = (i_fold + 1) * fold_len
+
+            A_fold_hat.append(self._convert_sp_mat_to_sp_tensor(X[start:end]))
+        return A_fold_hat
+
+    def _create_ngcf_embed(self):
+
+        A_fold_hat = self._split_A_hat(self.norm_adj)
+
+        ego_embeddings = tf.concat([self.weights['user_embedding'], self.weights['item_embedding']], axis=0)
+
+        for k in range(0, self.n_layers):
+
+            temp_embed = []
+            for f in range(self.n_fold):
+                temp_embed.append(tf.sparse_tensor_dense_matmul(A_fold_hat[f], ego_embeddings))
+
+            # sum messages of neighbors.
+            sum_embeddings = tf.concat(temp_embed, 0)
+
+            ego_embeddings = sum_embeddings
+
+        u_g_embeddings, i_g_embeddings = tf.split(ego_embeddings, [self.n_users, self.n_items], 0)
+        return u_g_embeddings, i_g_embeddings
+
+
+    def create_bpr_loss(self, users, pos_items, neg_items):
+        pos_scores = tf.reduce_sum(tf.multiply(users, pos_items), axis=1)
+        neg_scores = tf.reduce_sum(tf.multiply(users, neg_items), axis=1)
+
+        regularizer = tf.nn.l2_loss(users) + tf.nn.l2_loss(pos_items) + tf.nn.l2_loss(neg_items)
+
+        maxi = tf.log(tf.nn.sigmoid(pos_scores - neg_scores))
+        mf_loss = tf.negative(tf.reduce_mean(maxi))
+
+        emb_loss = self.decay * regularizer
+
+        reg_loss = tf.constant(0.0, tf.float32, [1])
+
+        return mf_loss, emb_loss, reg_loss
+
+    def _convert_sp_mat_to_sp_tensor(self, X):
+        coo = X.tocoo().astype(np.float32)
+        indices = np.mat([coo.row, coo.col]).transpose()
+        return tf.SparseTensor(indices, coo.data, coo.shape)
+
+    def _dropout_sparse(self, X, keep_prob, n_nonzero_elems):
+        """
+        Dropout for sparse tensors.
+        """
+        noise_shape = [n_nonzero_elems]
+        random_tensor = keep_prob
+        random_tensor += tf.random_uniform(noise_shape)
+        dropout_mask = tf.cast(tf.floor(random_tensor), dtype=tf.bool)
+        pre_out = tf.sparse_retain(X, dropout_mask)
+
+        return pre_out * tf.div(1., keep_prob)
+
+
+if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu_id)
+
+    config = dict()
+    config['n_users'] = data_generator.n_users
+    config['n_items'] = data_generator.n_items
+
+    """
+    *********************************************************
+    Generate the Laplacian matrix, where each entry defines the decay factor (e.g., p_ui) between two connected nodes.
+    """
+    plain_adj, norm_adj, mean_adj = data_generator.get_adj_mat()
+
+    if args.adj_type == 'plain':
+        config['norm_adj'] = plain_adj
+        print('use the plain adjacency matrix')
+
+    elif args.adj_type == 'norm':
+        config['norm_adj'] = norm_adj
+        print('use the normalized adjacency matrix')
+
+    elif args.adj_type == 'gcmc':
+        config['norm_adj'] = mean_adj
+        print('use the gcmc adjacency matrix')
+
+    else:
+        config['norm_adj'] = mean_adj + sp.eye(mean_adj.shape[0])
+        print('use the mean adjacency matrix')
+
+    t0 = time()
+
+    if args.pretrain == -1:
+        pretrain_data = load_pretrained_data()
+    else:
+        pretrain_data = None
+
+    model = RGCF(data_config=config, pretrain_data=pretrain_data)
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+
+
+    sess.run(tf.global_variables_initializer())
+    cur_best_pre_0 = 0.
+    print('without pretraining.')
+
+
+    loss_loger, pre_loger, rec_loger, ndcg_loger, hit_loger = [], [], [], [], []
+    stopping_step = 0
+    should_stop = False
+
+    for epoch in range(args.epoch):
+        t1 = time()
+        loss, mf_loss, emb_loss, reg_loss = 0., 0., 0., 0.
+        n_batch = data_generator.n_train // args.batch_size + 1
+
+        for idx in range(n_batch):
+            users, pos_items, neg_items = data_generator.sample()
+            _, batch_loss, batch_mf_loss, batch_emb_loss, batch_reg_loss = sess.run([model.opt, model.loss, model.mf_loss, model.emb_loss, model.reg_loss],
+                               feed_dict={model.users: users, model.pos_items: pos_items,
+                                          model.neg_items: neg_items})
+            loss += batch_loss
+            mf_loss += batch_mf_loss
+            emb_loss += batch_emb_loss
+            reg_loss += batch_reg_loss
+
+        if np.isnan(loss) == True:
+            print('ERROR: loss is nan.')
+            sys.exit()
+
+        # print the test evaluation metrics each 10 epochs; pos:neg = 1:10.
+        if (epoch + 1) % 10 != 0:
+            if args.verbose > 0 and epoch % args.verbose == 0:
+                perf_str = 'Epoch %d [%.1fs]: train==[%.5f=%.5f + %.5f]' % (
+                    epoch, time() - t1, loss, mf_loss, reg_loss)
+                print(perf_str)
+            continue
+
+        t2 = time()
+        users_to_test = list(data_generator.test_set.keys())
+        ret = test(sess, model, users_to_test, drop_flag=True)
+
+        t3 = time()
+
+        loss_loger.append(loss)
+        rec_loger.append(ret['recall'])
+        pre_loger.append(ret['precision'])
+        ndcg_loger.append(ret['ndcg'])
+        hit_loger.append(ret['hit_ratio'])
+
+        if args.verbose > 0:
+            perf_str = 'Epoch %d [%.1fs + %.1fs]: train==[%.5f=%.5f + %.5f + %.5f], recall=[%.5f, %.5f], ' \
+                       'precision=[%.5f, %.5f], hit=[%.5f, %.5f], ndcg=[%.5f, %.5f]' % \
+                       (epoch, t2 - t1, t3 - t2, loss, mf_loss, emb_loss, reg_loss, ret['recall'][0], ret['recall'][-1],
+                        ret['precision'][0], ret['precision'][-1], ret['hit_ratio'][0], ret['hit_ratio'][-1],
+                        ret['ndcg'][0], ret['ndcg'][-1])
+            print(perf_str)
+
+        cur_best_pre_0, stopping_step, should_stop = early_stopping(ret['recall'][0], cur_best_pre_0,
+                                                                    stopping_step, expected_order='acc', flag_step=5)
+
+        # *********************************************************
+        # early stopping when cur_best_pre_0 is decreasing for ten successive steps.
+        if should_stop == True:
+            break
+
+        # *********************************************************
+        # save the user & item embeddings for pretraining.
+        if ret['recall'][0] == cur_best_pre_0 and args.save_flag == 1:
+            save_saver.save(sess, weights_save_path + '/weights', global_step=epoch)
+            print('save the weights in path: ', weights_save_path)
+
+    recs = np.array(rec_loger)
+    pres = np.array(pre_loger)
+    ndcgs = np.array(ndcg_loger)
+    hit = np.array(hit_loger)
+
+    best_rec_0 = max(recs[:, 0])
+    idx = list(recs[:, 0]).index(best_rec_0)
+
+    final_perf = "Best Iter=[%d]@[%.1f]\trecall=[%s], precision=[%s], hit=[%s], ndcg=[%s]" % \
+                 (idx, time() - t0, '\t'.join(['%.5f' % r for r in recs[idx]]),
+                  '\t'.join(['%.5f' % r for r in pres[idx]]),
+                  '\t'.join(['%.5f' % r for r in hit[idx]]),
+                  '\t'.join(['%.5f' % r for r in ndcgs[idx]]))
+    print(final_perf)
+
+    save_path = '%soutput/%s.result' % (args.proj_path, args.dataset)
+    ensureDir(save_path)
+    f = open(save_path, 'a')
+
+    f.write(
+        'embed_size=%d, lr=%.4f, layer_size=%s, node_dropout=%s, mess_dropout=%s, regs=%s, loss_type=%s, adj_type=%s\n\t%s\n'
+        % (args.embed_size, args.lr, args.layer_size, args.node_dropout, args.mess_dropout, args.regs, args.loss_type,
+           args.adj_type, final_perf))
+    f.close()
